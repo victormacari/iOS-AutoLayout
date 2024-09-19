ce4137d36123f36aea461e69b60dd1ca781a2cf2
Victor Macari <victorm.macari@gmail.com>
2024-09-19 18:42:33 +0300

    Initial Commit

---
+++ Project6b/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project6b
+//
+//  Created by Victor Macari on 19.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
